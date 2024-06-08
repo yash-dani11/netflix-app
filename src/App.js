1b62b481edde6916a60599e948a2965c6fe61936
yashdani11 <yash.dani@timesinternet.in>
2024-06-09 02:16:04 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+const appRouter = createBrowserRouter([{
+  path:"/",
+  errorElement:<ErrorPage></ErrorPage>,
+  children:[{
+    path:"/",
+    element:,
+
+  }]
+}])
+
+export default App;
---
+++ src/ErrorPage.js
@@ -0,0 +1,5 @@
+const ErrorPage = ()=>{
+    return <>Error 404 | Page Not Found.</>
+}
+
+export default ErrorPage;
